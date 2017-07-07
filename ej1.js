dcfaf95f289e7e0ff35789481d57c8200985f695
luarar <laurarh.971@gmail.com>
2017-07-07 20:44:47 -0300

    cambios en el ejercicio

--- ej1.js
+++ ej1.js
@@ -1,6 +1,33 @@
-hola mundo
-1
-2
-23
-3
-4
+/* Write a program to calculate the total price of your phone purchase. 
+
+You will keep purchasing phones (hint: loop!) until you run out of money in your bank account. 
+
+You'll also buy accessories for each phone as long as your purchase amount is below your mental spending threshold.
+
+After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted.
+
+Finally, check the amount against your bank account balance to see if you can afford it or not.
+
+You should set up some constants for the "tax rate," "phone price," "accessory price," and "spending threshold," as well as a variable for your "bank account balance.""
+
+You should define functions for calculating the tax and for formatting the price with a "$" and rounding to two decimal places.
+
+Bonus Challenge: Try to incorporate input into this program, perhaps with the prompt(..) covered in "Input" earlier. 
+You may prompt the user for their bank account balance, for example. Have fun and be creative!*/
+
+const margen_gasto = 1000;
+const impuestos = 0.21;
+const precio_telefono = 99.99;
+const precio_accesorio = 9.99;
+
+var cantidadTelefonos = 0;
+
+var compra = (precio_telefono + precio_accesorio) * impuestos;
+	
+
+while (margen_gasto > 0) {
+	margen_gasto = margen_gasto - compra;
+    cantidadTelefonos++;
+};
+return "usted compró " + cantidadTelefonos + " teléfonos con el dinero disponible";
+
